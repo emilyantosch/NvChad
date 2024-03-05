97dffcd8f945ced11e02473642b49111c82d06a2
ericantosch <ericantosch@web.de>
2024-03-05 14:04:35 +0100

    Merge and added files

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,29 +4,14 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require "lspconfig/util"
 
-
 lspconfig.pyright.setup {
   on_attach = on_attach,
-  capabilities = capabilities
-}
-
-lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
   capabilities = capabilities,
-  filetypes = {"rust"},
-  root_dir = util.root_pattern("Cargo.toml"),
-  settings = {
-    ['rust_analyzer'] = {
-      cargo = {
-        allFeatures = true
-      }
-    }
-  }
 }
 
 lspconfig.marksman.setup {
   on_attach = on_attach,
-  capabilities = capabilities
+  capabilities = capabilities,
 }
 
 lspconfig.tsserver.setup {
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -20,7 +20,7 @@ local plugins = {
           ["core.integrations.telescope"] = {},
         },
       }
-    end, 
+    end,
   },
   {
   "neovim/nvim-lspconfig",
@@ -111,7 +111,25 @@ local plugins = {
         "pyright",
         "black",
       }
-    }  
+    }
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function (_, opts)
+      return require('rust-tools').setup(opts)
+    end
   }
 }
 
